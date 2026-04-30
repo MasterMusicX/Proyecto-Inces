8bb42345ea241029b0273d955af0d17d19bbbe11
MasterMusicX <josemigueldav05@gmail.com>
2026-04-30 08:33:49 -0400

    Cambios en base de datos

--- database/migrations/2026_04_06_095151_add_grades_and_gender_to_tables.php
+++ database/migrations/2026_04_06_095151_add_grades_and_gender_to_tables.php
@@ -8,12 +8,8 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // 1. Le agregamos el género a los usuarios
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('gender', 10)->nullable()->after('email'); // Puede ser 'M', 'F'
-        });
 
-        // 2. Le agregamos la nota y si aprobó a las inscripciones de los cursos
+        // 1. Le agregamos la nota y si aprobó a las inscripciones de los cursos
         Schema::table('enrollments', function (Blueprint $table) {
             $table->decimal('final_grade', 5, 2)->nullable()->after('progress_percentage');
             $table->boolean('is_approved')->default(false)->after('final_grade');
